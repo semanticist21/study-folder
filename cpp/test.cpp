41edb8c1690807a26a1ac222591cf0b97b41a75f
jiwon <semanticist21@github.com>
2023-08-27 15:12:34 +0900

    .

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -1,7 +1,33 @@
-#include <stdio.h>
+#include <iostream>
+namespace A_RANGE
+{
+    int score = 100;
+    int FUNC(int s)
+    {
+        return s / 5;
+    }
+}
+
+namespace B_RANGE
+{
+    int score = 200;
+    int FUNC(int s)
+    {
+        return s / 10;
+    }
+}
+
+using namespace std;
+using namespace A_RANGE;
+using namespace B_RANGE;
 
 int main()
 {
-    printf("Hello World!\n");
+    int score = 300;
+
+    cout << "main score : " << score << endl;
+    cout << "A_RANGE score : " << A_RANGE::score << endl;
+    cout << "B_RANGE score : " << B_RANGE::score << endl;
+
     return 0;
 }
