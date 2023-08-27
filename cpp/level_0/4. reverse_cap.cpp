4fe72369dc8f373217415311845ab403c7f7039a
jiwon <semanticist21@github.com>
2023-08-27 18:27:34 +0900

    .

---
+++ cpp/level_0/4. reverse_cap.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+int main(void)
+{
+  string str;
+  cin >> str;
+
+  string word;
+
+  for (char c : str)
+  {
+    if (isupper(c))
+    {
+      word += (char)tolower(c);
+      continue;
+    }
+
+    word += (char)toupper(c);
+  }
+
+  cout << word << endl;
+}
