25ff7c814b72bbafcae572c16b1d0da24f7ed3cf
jiwon <semanticist21@github.com>
2023-09-08 20:34:02 +0900

    .

---
+++ cpp/level_1/3.get_total.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(){
+  return 0;
+}
+
+int solution(vector<int> absolutes, vector<bool> signs) {
+    int item_len = absolutes.size();
+
+    for(int i=0;i<item_len;i++){
+      if(!signs[i]) absolutes[i] *= -1;
+    }
+
+    int answer = 0;
+
+    for(const int& value: absolutes){
+      answer += value;
+    }
+
+    return answer;
+}
