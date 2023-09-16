b3c2e2844de452caad2a1d00d334ac9b7ef72674
jiwon <semanticist21@github.com>
2023-09-16 23:34:20 +0900

    .

--- cpp/level_2/1.two_different_sphere.cpp
+++ cpp/level_2/1.two_different_sphere.cpp
@@ -1,22 +1,46 @@
 #include <string>
 #include <vector>
 #include <cmath>
+#include <iostream>
 
 using namespace std;
 
 long long solution(int r1, int r2)
 {
-  double pi = 2 * acos(0.0);
+  long r1_pow = r1 * (long)r1;
+  long r2_pow = r2 * (long)r2;
 
-  long long r1_dots = ceil(r1 * r1 * pi);
-  long long r2_dots = ceil(r2 * r2 * pi);
+  long on_line = 0;
+  long r1_dots = 0;
+  for (int i = 1; i < r1; ++i)
+  {
+    long i_pow = (long)i * i;
+    double sqrt_val = sqrt(r1_pow - i_pow);
 
-  long long result = r2_dots - r1_dots + 4;
+    if (fmod(sqrt_val, 1) == 0)
+    {
+      ++on_line;
+    }
 
-  return result;
+    r1_dots += floor(sqrt_val);
+  }
+
+  long r2_dots = 0;
+  for (int i = 1; i < r2; ++i)
+  {
+    long i_pow = (long)i * i;
+    r2_dots += floor(sqrt(r2_pow - i_pow));
+  }
+
+  long r1_total = r1_dots * 4 + r1 * 4 + 1;
+  long r2_total = r2_dots * 4 + r2 * 4 + 1;
+
+  return r2_total - r1_total + 4 + on_line * 4;
 }
 
 int main()
 {
+  auto result = solution(25, 39);
+  cout << result << endl;
   return 0;
 }
---
+++ cpp/test2.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+  const int test_val = 3;
+
+  int result = floor(test_val);
+
+  cout << result << endl;
+
+  return 0;
+}
