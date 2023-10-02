bf8c5f0cc8256a011261fbac01e8ef1091a61642
jiwon <semanticist21@github.com>
2023-10-02 20:22:07 +0900

    .

--- cpp/level_2/5.mine_minerals.cpp
+++ cpp/level_2/5.mine_minerals.cpp
@@ -3,14 +3,96 @@
 #include <string>
 #include <algorithm>
 #include <limits>
+#include <cmath>
+#include <map>
 
 using namespace std;
 
+int get_point(vector<string> &minerals)
+{
+    int point = 0;
+
+    const int weight_table[3] = {31, 6, 1};
+
+    for (const string &stone : minerals)
+    {
+        if (stone == "diamond")
+            point += weight_table[0];
+        else if (stone == "iron")
+            point += weight_table[1];
+        else
+            point += weight_table[2];
+    }
+
+    return point;
+}
+
 int solution(vector<int> picks, vector<string> minerals)
 {
+    int total = 0;
+    for (const int &pick : picks)
+    {
+        total += pick;
+    }
+
+    double mineral_cycle = static_cast<double>(minerals.size()) / 5;
+    int cycle = ceil(mineral_cycle) < total ? ceil(mineral_cycle) : total;
+
+    if (minerals.size() > cycle * 5)
+    {
+        minerals.erase(minerals.begin() + cycle * 5, minerals.end());
+    }
+
+    vector<vector<string>> w_map(cycle, vector<string>());
+
+    for (int i = 0; i < minerals.size(); i++)
+    {
+        w_map[i / 5].push_back(minerals[i]);
+    }
+
+    auto comp = [](vector<string> &a, vector<string> &b)
+    {
+        return get_point(a) > get_point(b);
+    };
+
+    sort(w_map.begin(), w_map.end(), comp);
+
+    const int dia_pick[3] = {1, 1, 1};
+    const int iron_pick[3] = {5, 1, 1};
+    const int stone_pick[3] = {25, 5, 1};
+
+    int cost = 0;
+    for (const vector<string> &pair : w_map)
+    {
+        const int *table;
+        if (picks[0] > 0 && picks[0]--)
+            table = dia_pick;
+        else if (picks[1] > 0 && picks[1]--)
+            table = iron_pick;
+        else
+            table = stone_pick;
+
+        for (const string &mineral : pair)
+        {
+            if (mineral == "diamond")
+                cost += table[0];
+            else if (mineral == "iron")
+                cost += table[1];
+            else
+                cost += table[2];
+        }
+    }
+
+    return cost;
 }
 
 int main()
 {
+    vector<int> picks = {1, 3, 2};
+    vector<string> minerals = {"diamond", "diamond", "diamond", "iron", "iron", "diamond", "iron", "stone"};
+
+    int min = solution(picks, minerals);
+    cout << min << endl;
+
     return 0;
 }
