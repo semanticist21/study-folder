1b5523e6c04968551551b8d9dc2b8a49e53dda3d
jiwon <semanticist0@gmail.com>
2023-11-11 18:46:24 +0900

    🚧

--- cpp/level_2/20.make_natural_number/solution.cpp
+++ cpp/level_2/20.make_natural_number/solution.cpp
@@ -1,32 +1,33 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <limits>
 
 using namespace std;
 
-void iter_operation(int cur, int op, int &target, int &adder, vector<int> &ops)
+void iter_operation(int cur, int op, int &target, int &adder, int &min)
 {
     if (cur >= target)
     {
-        if (cur == target)
-            ops.emplace_back(op);
+        if (cur == target && op < min)
+            min = op;
         return;
     }
 
-    iter_operation(cur + adder, ++op, target, adder, ops);
-    iter_operation(cur * 2, op, target, adder, ops);
-    iter_operation(cur * 3, op, target, adder, ops);
+    iter_operation(cur + adder, ++op, target, adder, min);
+    iter_operation(cur * 2, op, target, adder, min);
+    iter_operation(cur * 3, op, target, adder, min);
 }
 
 int solution(int x, int y, int n)
 {
-    vector<int> ops;
-    iter_operation(x, 0, y, n, ops);
+    int max_val = numeric_limits<int>::max();
+    int min = max_val;
+    iter_operation(x, 0, y, n, min);
 
-    return ops.empty() ? -1 : *min_element(ops.begin(), ops.end());
+    return min == max_val ? -1 : min;
 }
 
-int main()
-{
+int main(){
     return 0;
 }
