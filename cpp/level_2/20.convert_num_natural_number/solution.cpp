e92729f91dd643039f17c9d098b3b5373f8c2691
jw <semanticist21@github.com>
2023-11-29 22:24:20 +0900

    ✨

--- cpp/level_2/20.convert_num_natural_number/solution.cpp
+++ cpp/level_2/20.convert_num_natural_number/solution.cpp
@@ -2,24 +2,42 @@
 #include <vector>
 #include <algorithm>
 #include <limits>
+#include <unordered_set>
 
 using namespace std;
 
 int solution(int x, int y, int n)
 {
-    int cur = x;
-    int cnt = 0;
+    unordered_set<int> sums;
+    unordered_set<int> new_sums;
+    sums.insert(x);
 
-    while(cur < y){
-        if(y%(cur*3) == 0 && cur+n < cur * 3) cur*=3;
-        else if(y%(cur*2) == 0 && cur+n <= cur * 2) cur*= 2;
-        else cur += n;
+    int answer = 0;
 
-        cnt++;
+    if(x == y) return 0;
+
+    while(!sums.empty()){
+        ++answer;
+
+        for(const int prev_sum : sums){
+            if(prev_sum > y) continue;
+
+            new_sums.insert(prev_sum*2);
+            new_sums.insert(prev_sum*3);
+            new_sums.insert(prev_sum+n);
+        }
+
+        sums.clear();
+
+        for(const int new_sum : new_sums){
+            if(new_sum == y) return answer;
+            else sums.insert(new_sum);
+        }
+
+        new_sums.clear();
     }
 
-    if(cur != y) return -1;
-    else return cnt;
+    return -1;
 }
 
 int main()
