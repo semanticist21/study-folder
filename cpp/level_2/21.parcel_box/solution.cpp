dc60fbd9a9fde569f6705e69b4e828975dc947b8
jiwon <semanticist0@gmail.com>
2023-12-03 17:30:19 +0900

    🚧

---
+++ cpp/level_2/21.parcel_box/solution.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> order)
+{
+    vector<int> subs;
+    int lastest_target = 1;
+
+    for (const int &parcel : order)
+    {
+    }
+}
+
+int main()
+{
+    return 0;
+}
