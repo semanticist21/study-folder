f04dbe254aadb2b2f836cc9027c22c7c9bdd3a28
jiwon <semanticist0@gmail.com>
2024-02-22 22:36:00 +0900

    🚧 merge_two_sorted_aray

---
+++ rust/src/solutions/e3_merge_two_sorted_aray/mod.rs
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/merge-two-sorted-lists/
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
+}
+
+pub fn merge_two_lists(
+    list1: Option<Box<ListNode>>,
+    list2: Option<Box<ListNode>>,
+) -> Option<Box<ListNode>> {
+}
--- rust/src/solutions/mod.rs
+++ rust/src/solutions/mod.rs
@@ -3,3 +3,4 @@ pub mod m2_3_sum_closest;
 
 pub mod e1_invert_int_32;
 pub mod e2_valid_parentheses;
+pub mod e3_merge_two_sorted_aray;
