9801178e3f597bdcc2e71cf889bd874af8b25e52
jiwonPark <semanticist0@gmail.com>
2024-02-25 23:51:23 +0900

    🚧

--- rust/src/solutions/e3_merge_two_sorted_aray/mod.rs
+++ rust/src/solutions/e3_merge_two_sorted_aray/mod.rs
@@ -16,4 +16,64 @@ pub fn merge_two_lists(
     list1: Option<Box<ListNode>>,
     list2: Option<Box<ListNode>>,
 ) -> Option<Box<ListNode>> {
+    let mut list1 = list1;
+    let mut list2 = list2;
+
+    let mut new_list = Box::new(ListNode::new(0));
+    let mut cur_node = new_list.as_mut();
+
+    while list1.is_some() && list2.is_some() {
+        if list1.is_some() && list2.is_none() {
+            let list1_val = list1.as_ref().unwrap().val;
+            let new_node = ListNode::new(list1_val);
+
+            cur_node.next = Some(Box::new(new_node));
+            cur_node = cur_node.next.as_ref().unwrap();
+        }
+
+        if list2.is_some() && list1.is_none() {
+            
+        }
+
+    }
+
+     None   
+}
+
+#[test]
+fn test() {
+    let l1 = Some(Box::new(ListNode {
+        val: 1,
+        next: Some(Box::new(ListNode {
+            val: 2,
+            next: Some(Box::new(ListNode { val: 4, next: None })),
+        })),
+    }));
+
+    let l2 = Some(Box::new(ListNode {
+        val: 1,
+        next: Some(Box::new(ListNode {
+            val: 3,
+            next: Some(Box::new(ListNode { val: 4, next: None })),
+        })),
+    }));
+
+    let expected = Some(Box::new(ListNode {
+        val: 1,
+        next: Some(Box::new(ListNode {
+            val: 1,
+            next: Some(Box::new(ListNode {
+                val: 2,
+                next: Some(Box::new(ListNode {
+                    val: 3,
+                    next: Some(Box::new(ListNode {
+                        val: 4,
+                        next: Some(Box::new(ListNode { val: 4, next: None })),
+                    })),
+                })),
+            })),
+        })),
+    }));
+
+    assert_eq!(merge_two_lists(l1, l2), expected);
 }
