e3dc6e11f0f4ffb9426de7f7396aff6d9bcabd8e
jiwonPark <semanticist0@gmail.com>
2024-03-03 20:50:43 +0900

    🚧

--- rust/src/solutions/e3_merge_two_sorted_aray/mod.rs
+++ rust/src/solutions/e3_merge_two_sorted_aray/mod.rs
@@ -1,3 +1,5 @@
+use std::rc::Rc;
+
 // https://leetcode.com/problems/merge-two-sorted-lists/
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub struct ListNode {
@@ -16,28 +18,7 @@ pub fn merge_two_lists(
     list1: Option<Box<ListNode>>,
     list2: Option<Box<ListNode>>,
 ) -> Option<Box<ListNode>> {
-    let mut list1 = list1;
-    let mut list2 = list2;
-
-    let mut new_list = Box::new(ListNode::new(0));
-    let mut cur_node = new_list.as_mut();
-
-    while list1.is_some() && list2.is_some() {
-        if list1.is_some() && list2.is_none() {
-            let list1_val = list1.as_ref().unwrap().val;
-            let new_node = ListNode::new(list1_val);
-
-            cur_node.next = Some(Box::new(new_node));
-            cur_node = cur_node.next.as_ref().unwrap();
-        }
-
-        if list2.is_some() && list1.is_none() {
-            
-        }
-
-    }
-
-     None   
+   None
 }
 
 #[test]
