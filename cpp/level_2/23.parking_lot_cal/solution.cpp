706f2768694c081e928608af1b455979b7b9a0e0
jiwonPark <semanticist0@gmail.com>
2024-03-11 23:57:49 +0900

    🚧

--- cpp/level_2/23.parking_lot_cal/solution.cpp
+++ cpp/level_2/23.parking_lot_cal/solution.cpp
@@ -1,13 +1,28 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
+#include <sstream>
 
 using namespace std;
 
 vector<int> solution(vector<int> fees, vector<string> records)
 {
-    vector<int> answer;
-    return answer;
+    unordered_map<int, string> car_in;
+    unordered_map<int, int> fee_paid;
+
+    for(const string info : records){
+        istringstream iss(info);
+        string buffer;
+        
+        vector<string> rs;
+
+        while(getline(iss, buffer, ' ')){
+            result.emplace_back(buffer);
+        }
+
+    }
+    
+
 }
 
 int main()
