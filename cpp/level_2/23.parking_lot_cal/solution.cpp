d903923dd0623a7c8c24b79e07235f8157947d42
jiwon <semanticist0@gmail.com>
2024-03-11 22:53:17 +0900

    🚧

---
+++ cpp/level_2/23.parking_lot_cal/solution.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+vector<int> solution(vector<int> fees, vector<string> records)
+{
+    vector<int> answer;
+    return answer;
+}
+
+int main()
+{
+}
